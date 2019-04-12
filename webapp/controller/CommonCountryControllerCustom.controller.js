dd35ba7d5b4ea35a571f070e8c53e1451101e07d
Venkata Ramana Palley <rap2015@med.cornell.edu>
2019-04-12 15:48:31 +0000

    Initial

--- webapp/controller/CommonCountryControllerCustom.controller.js
+++ webapp/controller/CommonCountryControllerCustom.controller.js
@@ -20,6 +20,27 @@ sap.ui.controller("hcm.fab.mybankdetails.ZHCMFAB_BANK_MAN_EX.controller.CommonCo
 		var selId = this.createId("selPaymentMethodId");
 		var selPmMtd = this.byId(selId);
 		var selKey = selPmMtd.getSelectedKey();
+		
+		var rNumId = this.createId("selBankControlKey");
+		var rNumber = this.byId(rNumId);
+		var oModel = this.getView().getModel("ValueHelpBankControlKey");
+		if (!oModel) {
+			if (rNumber.getItems().length === 0) {
+				rNumber.addItem(new sap.ui.core.Item({
+					key: "",
+					text: ""
+				}));
+				rNumber.addItem(new sap.ui.core.Item({
+					key: "01",
+					text: "Checking"
+				}));
+				rNumber.addItem(new sap.ui.core.Item({
+					key: "02",
+					text: "Savings"
+				}));
+			}
+		}
+		
 		if (selKey === "K") {
 			var bnkKey = this.byId(this.createId("inpBankId"));
 			var bnkCtrlK = this.byId(this.createId("selBankControlKey"));
@@ -32,6 +53,30 @@ sap.ui.controller("hcm.fab.mybankdetails.ZHCMFAB_BANK_MAN_EX.controller.CommonCo
 			bnkCtrlK.setVisible(false);
 			bnkAcN.setVisible(false);
 		}
+// removing Country selector form which record type selection
+        var pItems = this.oView.getParent().getAggregation("items");
+        pItems.forEach(function (form, indx) {
+            var formId = form.getId();
+            if (formId.indexOf("CountrySelectorForm") > -1) {
+                if (form.getTitle()) {
+                    form.destroyTitle();
+                }
+                var fContainer = form.getAggregation("form").getAggregation("formContainers");
+                var fElements = fContainer[0].getAggregation("formElements");
+                fElements.forEach(function (elem) {
+                    var field = elem.getAggregation("fields");
+                    field[0].setVisible(false);
+                    var label = elem.getAggregation("label");
+                    label.setVisible(false);
+                });
+            }
+        });
+        //removing Other Bank title
+        var obTitle = this.getView().getContent()[0].getTitle();
+        if (obTitle) {
+            this.getView().getContent()[0].destroyTitle();
+        }		
+		
 	},
 
 	onPaymentMethodChg: function (e) {
--- webapp/controller/MainCustom.controller.js
+++ webapp/controller/MainCustom.controller.js
@@ -2,56 +2,86 @@ sap.ui.controller("hcm.fab.mybankdetails.ZHCMFAB_BANK_MAN_EX.controller.MainCust
 
 	extHookGetCustomEntityNameForVersionId: function (v) {
 		// Place your hook implementation code here 
-		var btnId = this.createId("BankDetails--displayHeader--changePictureBtn");
-		var chgPicBtn = this.byId(btnId);
-		chgPicBtn.setVisible(false);
-
-		//Rename Create Button
-		var cBtnId = this.createId("BankDetails--displayHeader--createRecordBtn");
-		var creatBtn = this.byId(cBtnId);
-		creatBtn.setText("Add Account");
-
+		var i18nz = this.getModel("i18nz");
 		//Adding text element.
 		var dObjLayoutId = this.createId("BankDetails--displayObjectPageLayout");
 		var dObjLayout = this.byId(dObjLayoutId);
 		var dObjLayoutC = dObjLayout.getHeaderContent();
-		var textAvail = dObjLayoutC.find(function (ctrl, index) {
-			if (ctrl.getId() === "HBox1") {
-				return ctrl;
-			} else {
-				ctrl.setVisible(false);
-			}
-		});
+		var objPageTitle = dObjLayout.getHeaderTitle();
 
+		var i = 0,
+			textAvail, aLength = dObjLayoutC.length;
+		for (; i < aLength; i++) {
+			if (dObjLayoutC[i].getId() === "HBox1") {
+				textAvail = dObjLayoutC[i];
+			var msgstrip, oDummyText1;
+				msgstrip = dObjLayoutC[i].getFixContent()[0];
+				var oJModel = new sap.ui.model.json.JSONModel();
+				oJModel.loadData("/sap/opu/odata/sap/ZHPY_BANK_UTIL_SRV/PyData", "", false);
+				var oData2 = oJModel.getData();
+				var msgType = msgstrip.getType();
+				var layout1;
+				if (oData2.d.ZpyLock === true && msgType === "Information") {
+					//remove info add error
+					layout1 = msgstrip.getParent();
+					layout1.removeAllFixContent();
+
+					oDummyText1 = new sap.m.MessageStrip({
+						type: "Error",
+						enableFormattedText: true,
+						showIcon: true,
+						text: "{i18nz>headerPyLkError}"
+					}).addStyleClass("sapUiTinyMarginBegin");
+					layout1.addFixContent(oDummyText1);
+				} else if (oData2.d.ZpyLock === false && msgType === "Error") {
+					//remove error add info
+					layout1 = msgstrip.getParent();
+					layout1.removeAllFixContent();
+
+					var datev1 = oData2.d.ZpyCurrPydt.slice(4, 6) + "/" + oData2.d.ZpyCurrPydt.slice(6, 8) + "/" + oData2.d.ZpyCurrPydt.slice(0, 4);
+					oDummyText1 = new sap.m.MessageStrip({
+						showIcon: true,
+						text: "{i18nz>headerEffDtInfo1} " + datev1 + "." + " {i18nz>headerEffDtInfo2}"
+					}).addStyleClass("sapUiTinyMarginBegin");
+					layout1.addFixContent(oDummyText1);
+				}	
+				
+			} else {
+				dObjLayoutC[i].setVisible(false);
+			}
+		}
 		if (!textAvail) {
+			//Remove Picture Button
+			var btnId = this.createId("BankDetails--displayHeader--changePictureBtn");
+			var chgPicBtn = this.byId(btnId);
+			if (chgPicBtn) {
+				chgPicBtn.setVisible(false);
+			}
+
+			//Rename Create Button
+			var cBtnId = this.createId("BankDetails--displayHeader--createRecordBtn");
+			var creatBtn = this.byId(cBtnId);
+			creatBtn.setText("Add Account");
+
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.loadData("/sap/opu/odata/sap/ZHPY_BANK_UTIL_SRV/PyData", "", false);
 			var oData = oModel.getData();
-			var oDummyText, oIcon;
+			var oDummyText;
 
 			if (oData.d.ZpyLock === true) {
 				//Payroll Lock
 				oDummyText = new sap.m.MessageStrip({
 					type: "Error",
 					showIcon: true,
-					text: "Payroll is in process, To make any changes please come back later"
+					text: "{i18nz>headerPyLkError}"
 				}).addStyleClass("sapUiTinyMarginBegin");
-				// oIcon = new sap.ui.core.Icon({
-				// 	src: "sap-icon://error",
-				// 	width: "1rem",
-				// 	color: "Negative"
-				// });
+
 			} else {
-				var datev = oData.d.ZpyNextBegda.slice(4, 6) + "/" + oData.d.ZpyNextBegda.slice(6, 8) + "/" + oData.d.ZpyNextBegda.slice(0, 4);
+				var datev = oData.d.ZpyCurrPydt.slice(4, 6) + "/" + oData.d.ZpyCurrPydt.slice(6, 8) + "/" + oData.d.ZpyCurrPydt.slice(0, 4);
 				oDummyText = new sap.m.MessageStrip({
 					showIcon: true,
-					text: "Any changes that you make today will be effective from: " + datev
+					text: "{i18nz>headerEffDtInfo1} " + datev + "." + " {i18nz>headerEffDtInfo2}"
 				}).addStyleClass("sapUiTinyMarginBegin");
-				// oIcon = new sap.ui.core.Icon({
-				// 	src: "sap-icon://message-information",
-				// 	width: "1rem",
-				// 	color: "Negative"
-				// });
 			}
 
 			var oHBox = new sap.ui.layout.FixFlex("HBox1", {
@@ -62,51 +92,31 @@ sap.ui.controller("hcm.fab.mybankdetails.ZHCMFAB_BANK_MAN_EX.controller.MainCust
 			oHBox.addFixContent(oDummyText);
 			dObjLayout.addHeaderContent(oHBox);
 
-			// });
+			// giving error more than 4 banks.
+			if (objPageTitle.getActions().length > 0) {
+				var funName = creatBtn.mEventRegistry.press[0].fFunction._sapui_handlerName;
+				if (funName === "onCreate") {
+					var bdid = this.createId("BankDetails");
+					var bd = this.byId(bdid);
+					creatBtn.detachPress(bd.onCreate, bd);
+
+					var customCreate = function customCreate(oEvent) {
+						var oModel1 = new sap.ui.model.json.JSONModel();
+						oModel1.loadData("/sap/opu/odata/sap/ZHPY_BANK_UTIL_SRV/PyData", "", false);
+						var oData1 = oModel1.getData();
+						if (oData1.d.ZpyMaxBanks === true) {
+							var msg = i18nz.getProperty("maxBankError");
+							sap.m.MessageBox.error(msg);
+						} else {
+							bd.onCreate();
+						}
+
+					};
+					creatBtn.attachPress(customCreate);
+				}
+			}
 
 		}
-		// giving error more than 4 banks.
-			var funName = creatBtn.mEventRegistry.press[0].fFunction._sapui_handlerName;
-			if (funName === "onCreate") {
-				var bdid = this.createId("BankDetails");
-				var bd = this.byId(bdid);
-				creatBtn.detachPress(bd.onCreate, bd);
-
-				var customCreate = function customCreate(oEvent) {
-					var oModel1 = new sap.ui.model.json.JSONModel();
-					oModel1.loadData("/sap/opu/odata/sap/ZHPY_BANK_UTIL_SRV/PyData", "", false);
-					var oData1 = oModel1.getData();
-					if (oData1.d.ZpyMaxBanks === true) {
-						sap.m.MessageBox.error("You have already reached the max number of secondary banks i.e 4");
-					} else {
-						bd.onCreate();
-					}
-
-				};
-				creatBtn.attachPress(customCreate);
-			}
-		//ObjectPage Header 
-		// var oPHId = this.createId("BankDetails--displayHeader--mainPageHeaderTitle");
-		// var oPHeader = this.byId(oPHId);
-		//giving error more than 4 banks.
-		// if (creatBtn.getVisible() === true) {
-		// if (oPHeader.getActions().length >= 4) {
-		// 	return;
-		// } else {
-		// 	function onDummyAdd(oEvent) {
-		// 		new sap.m.MessageBox.error("You have already reached the max number of secondary banks i.e 4");
-		// 	}
-		// 	var oAdd2 = new sap.uxap.ObjectPageHeaderActionButton("Add2", {
-		// 		text: "Add Account2",
-		// 		type: "Emphasized",
-		// 		hideText: false,
-		// 		press: onDummyAdd,
-		// 		visible: false
-		// 	});
-		// 	oPHeader.addAction(oAdd2);
-		// }
-		// }
-
 	}
 
 });
